58ccb5dcde3d5f7d70e222da0d3141746faf3462
Elayabharath Elango <elayabharath@gmail.com>
2020-06-04 17:41:39 +0800

    Util methods for login modal domains, button

--- lib/utils.dart
+++ lib/utils.dart
@@ -3,12 +3,17 @@ import 'dart:html' as html;
 const LOCATION_EVENT_NAME = 'locationChanged';
 const LOADING_MODAL_ID = 'loading-modal';
 const LOGIN_MODAL_ID = 'login-modal';
+const LOGIN_EMAIL_DOMAINS_SPAN_ID = 'login-email-domains';
 const LOGIN_ERROR_ALERT_ID = 'login-error';
+const LOGIN_BUTTON_ID = 'login-button';
 
 html.DivElement get loadingModal => html.querySelector('#${LOADING_MODAL_ID}');
 html.DivElement get loginModal => html.querySelector('#${LOGIN_MODAL_ID}');
+html.DivElement get loginEmailDomains =>
+    html.querySelector('#${LOGIN_EMAIL_DOMAINS_SPAN_ID}');
 html.DivElement get loginErrorAlert =>
     html.querySelector('#${LOGIN_ERROR_ALERT_ID}');
+html.ButtonElement get loginButton => html.querySelector('#${LOGIN_BUTTON_ID}');
 
 // Route utils
 String get currentPathname => html.window.location.pathname;
@@ -45,6 +50,20 @@ void showLoginModal() {
 
 void hideLoginModal() {
   loginModal.setAttribute('hidden', 'true');
+}
+
+void setLoginDomains(List<String> domains) {
+  loginEmailDomains.innerText = domains.join(', ');
+}
+
+void enableLoginButton() {
+  loginButton.removeAttribute('disabled');
+  loginButton.innerText = 'Sign in with Google';
+}
+
+void disableLoginButton() {
+  loginButton.setAttribute('disabled', 'true');
+  loginButton.innerText = 'Signing in ...';
 }
 
 void showLoginError(String message) {
