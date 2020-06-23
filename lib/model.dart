5201647cd2c5ed93d9678689ce2660ebaf9f010a
Elayabharath Elango <elayabharath@gmail.com>
2020-06-23 18:57:16 +0800

    Remove unused TickOptions from model

--- lib/model.dart
+++ lib/model.dart
@@ -1,5 +1,4 @@
 export 'package:dashboard/model.g.dart';
-import 'package:chartjs/chartjs.dart' as chartjs;
 
 class Link {
   String pathname;
@@ -7,18 +6,4 @@ class Link {
   void Function() render;
 
   Link(this.pathname, this.label, this.render);
-}
-
-class TickOptions implements chartjs.TickOptions {
-  num minValue;
-
-  @override
-  set min(dynamic v) {
-    minValue = v as num;
-  }
-
-  @override
-  num get min => minValue;
-
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
