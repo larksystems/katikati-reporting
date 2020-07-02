c5b3117492857dca725a8ca3980335b71f1fe7fc
Elayabharath Elango <elayabharath@gmail.com>
2020-07-02 16:38:36 +0800

    Update dev.dart

--- dev.dart
+++ dev.dart
@@ -1,29 +1,40 @@
 import 'dart:convert';
 import 'dart:io';
 
+Future<File> fetchConfigFile(String path, String type) async {
+  var file = File(path);
+  if (!await file.exists()) {
+    throw ArgumentError('${type} config file does not exist at: ${path}');
+  }
+
+  return file;
+}
+
 void main(List<String> arguments) async {
-  if (arguments == null) {
-    throw ArgumentError('dart dev.dart <proj-name-from-project_config.json>');
-  }
-
-  var baseConfigFile = File('config/base_config.json');
-  var projConfigFile = File('config/project_config.json');
-  if (!await baseConfigFile.exists() || !await projConfigFile.exists()) {
-    throw ArgumentError('Config files not present');
-  }
-
+  var baseConfigFile = await fetchConfigFile('config/base_config.json', 'base');
   var baseConfigRaw = await baseConfigFile.readAsString();
   Map<String, dynamic> baseConfig = jsonDecode(baseConfigRaw);
+
+  var projConfigFile =
+      await fetchConfigFile('config/project_config.json', 'project');
   var projConfigRaw = await projConfigFile.readAsString();
   Map<String, dynamic> projConfig = jsonDecode(projConfigRaw);
 
-  var projName = arguments[0];
+  var projName;
 
-  if (projName == null || projConfig[projName] == null) {
-    throw StateError('No such project found in project_config.json');
+  if (arguments == null) {
+    projName = projConfig.keys.first;
+    print(
+        '''No project specified. Usage: dart dev.dart <proj-name-from-project_config.json>\nConsidering the first project under config/project_config ** ${projName} **''');
+  } else {
+    projName = arguments[0];
+    if (projName == null || projConfig[projName] == null) {
+      throw StateError(
+          'No such ${projName} project found in project_config.json');
+    }
   }
 
   var newConfig = {...baseConfig, ...projConfig[projName]};
-  var newFile = File('web/assets/firebase-constants.json');
+  var newFile = File('web/assets/constants.json');
   newFile.writeAsStringSync(jsonEncode(newConfig));
 }
